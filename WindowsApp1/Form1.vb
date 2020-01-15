fa475589af5c9e99711eb9bc1817c24423f53e58
ekaitz zuazo <ekaitz.zuazopi@elorrieta-errekamari.com>
2020-01-15 12:53:50 +0100

    segundo boton

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hola Elorrieta")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Hola David")
+    End Sub
 End Class
